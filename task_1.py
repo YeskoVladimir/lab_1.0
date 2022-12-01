159171566f873f90f8e5ced3cc337f4fda0078e5
Еcьков Владимир <veskov>
2022-12-01 22:34:23 +0300

    task_1 fix

--- task_1.py
+++ task_1.py
@@ -7,32 +7,19 @@ one_count = 0
 
 def main(args):
     global zero_count, one_count
-    random_list = [random.choice([0, 1]) for _ in range(args.n)]
+    random_list = [random.choice(['0', '1']) for _ in range(args.n)]
+    list_as_string = ''.join(x for x in random_list)
 
-    unique_value_count = 0
-    is_zero_or_one(random_list[0])
-    is_zero_or_one(random_list[-1])
+    for i in range(1, len(random_list) + 1):
+        zero_string = "0" * i
+        print(f'{zero_string}: {round(list_as_string.count(zero_string) * 100 / (len(random_list) - i + 1), 2)} %')
+        one_string = "1" * i
+        print(f'{one_string}: {round(list_as_string.count(one_string) * 100 / (len(random_list) - i + 1), 2)} %')
 
-    for i in range(1, len(random_list)-1):
-        if random_list[i] != random_list[i-1] and random_list[i] != random_list[i+1]:
-            unique_value_count += 1
-        is_zero_or_one(random_list[i])
-
-    print(f'zero percentage: {zero_count * 100 / args.n}%')
-    print(f'one percentage: {one_count * 100 / args.n}%')
-    print(f'duplicated value: {len(random_list) - unique_value_count}')
-
-
-def is_zero_or_one(value: int):
-    global zero_count, one_count
-    if value == 0:
-        zero_count += 1
-    else:
-        one_count += 1
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--n", help="List size", type=int, default=1000)
+    parser.add_argument("--n", help="List size", type=int, default=100)
 
     main(parser.parse_args())
